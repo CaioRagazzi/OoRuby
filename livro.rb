ea380ae5ab64ac2ccdc500b5d04734cc1ce4d406
Caio <ca.ragazzi@gmail.com>
2017-04-04 01:55:08 -0300

    v2

--- livro.rb
+++ livro.rb
@@ -1,4 +1,26 @@
 class Livro
 
+	attr_reader :titulo, :preco, :ano_lancamento
+
+	def initialize(titulo, preco, ano_lancamento)
+	    @titulo = titulo
+	    @preco = preco
+	    @ano_lancamento = ano_lancamento
+  	end
 
 end
+
+	livro_rails = Livro.new("Agile Web Development with Rails",70,2011)
+	livro_ruby = Livro.new("Programming Ruby 1.9",60,2010)
+
+	livros = [livro_ruby, livro_rails]
+
+	def imprime_nota_fiscal(livros)
+		livros.each do |livro|
+			livro.preco = 1.00
+			puts "Titulo: #{livro.titulo} - #{livro.preco}"
+		end
+	end
+
+
+	imprime_nota_fiscal(livros)
